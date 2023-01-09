7f61047651aea70948b0ca728aad301a184ef7f6
espeon <danielpacheco842005@gmail.com>
2023-01-09 12:29:05 -0500

    Factura

--- CatalogoCompras/src/app/catalogo/catalogo.component.ts
+++ CatalogoCompras/src/app/catalogo/catalogo.component.ts
@@ -12,14 +12,40 @@ export class CatalogoComponent implements OnInit{
   preciototal: any;
   precio1:any
   total: number=0
+  nombre:any
+  objeto1= "Audifono con microfonos--------$150"
+  objeto2= "Celular Xiaomi--------$500"
+  objeto3= "Teclado Gamer--------$200"
 
   ngOnInit(): void {
     localStorage.clear()
+    this.agregar1()
   }
 
   agregar1(){
-    this.preciototal=this.preciototal+this.precio1;
+    this.nombre=localStorage.getItem('valor_nombre')
+
   }
+  item1(){
+    localStorage.setItem('valor_item1',this.objeto1)
+  }
+  item2(){
+    localStorage.setItem('valor_item2',this.objeto2)
+  }
+  item3(){
+    localStorage.setItem('valor_item3',this.objeto1)
+  }
+
+  limpiar1(){
+    localStorage.removeItem(this.objeto1)
+  }
+  limpiar2(){
+    localStorage.removeItem(this.objeto2)
+  }
+  limpiar3(){
+    localStorage.removeItem(this.objeto3)
+  }
+
 
   logout(){
     this.router.navigate(['cerrar'])
--- CatalogoCompras/src/app/factura/factura.component.ts
+++ CatalogoCompras/src/app/factura/factura.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-factura',
   templateUrl: './factura.component.html',
   styleUrls: ['./factura.component.css']
 })
-export class FacturaComponent {
+export class FacturaComponent implements OnInit {
+  constructor(private router: Router){}
+  ngOnInit(): void {
+    this.obtencion1()
+  }
+
+  item1: any;
+  item2: any;
+  item3: any;
+
+  obtencion1(){
+    this.item1=localStorage.getItem('valor_item1');
+    this.item2=localStorage.getItem('valor_item2');
+    this.item3=localStorage.getItem('valor_item3');
+  }
+
+  regresar(){
+    this.router.navigate(['login'])
+  }
+  logout(){
+    this.router.navigate(['cerrar'])
+  }
 
 }
